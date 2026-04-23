a491e3872263d1a3fd81b42c29d7843005eb81de
Vitaliy Puzyryov <v.puziryov@gmail.com>
2026-04-23 13:18:55 +0300

    23/04-24

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-self.addEventListener('install', () => {
+self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', () => {});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // пока без кэша
+});
